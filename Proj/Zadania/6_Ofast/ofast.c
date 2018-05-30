c271a09a3cf7d22391fe5c97544659336c7517fc
Wojciech Franczuk <33672959+FearlessPoro@users.noreply.github.com>
2018-05-30 09:48:06 +0200

    Update ofast.c

--- Proj/Zadania/6_Ofast/ofast.c
+++ Proj/Zadania/6_Ofast/ofast.c
@@ -11,10 +11,5 @@ double f(double x, double y, double z, double w)
 /*
 1) Porównaj kod assemblerowy przy użyciu flag O0, O3,oraz Ofast, jakie widać różnice? Czy taka optymalizacja jest pożądana?
 2) Zaproponuj takie wartości x, y, z, oraz w wewnątrz programu main.c, aby wynik obliczeń z flagą Ofast był niepoprawny, natomiast z O3 tak
-(Przykładowa odpowiedź:
-	double x = 1.;
-	double y = -1.;
-	double z = 1e-20;
-	double w = 1.;
-)
+
 */
