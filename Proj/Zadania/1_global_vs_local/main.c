14b143f625a062872b5a80947c467cb62818f41b
Wojciech Franczuk <33672959+FearlessPoro@users.noreply.github.com>
2018-05-30 09:45:48 +0200

    usunięcie odpowiedzi

--- Proj/Zadania/1_global_vs_local/main.c
+++ Proj/Zadania/1_global_vs_local/main.c
@@ -14,7 +14,7 @@ int main()
 
 /*
 1) Sprawdź kod maszynowy powstały z kompilacji programu (np. zatrzymaj gcc przed wykonaniem asemblacji, użyj objdump lub wejdź na stronę godbolt) 
-2) Sprawdź w jaki sposób kompilator obsługuje zmienne lokalne oraz globalne (gdzie oraz w jaki sposób są przechowywane, oraz jak są obsługiwane w trakcie trwania programu) (odpowiedź: zmienne globalne są zawsze trzymane osobno w sekcji .data, lokalne wrzucane są na stos)
-3) Dodaj flagę optymalizacji np. O3 - czy są widoczne różnice pomiędzy traktowaniem zmiennych globalnych oraz lokalnych? (odpowiedź: lokalne zmienne mogą być ładowane od razu do rejestrów, jako stałe, brak wrzucania na stos)
-4) Zamień zmienne globalne na lokalne, sprawdź kod assemblera z włączoną flagą O3, które linie kodu zostały wykonane w trakcie kompilacji? (odpowiedź: wszystkie poza printf, kompilator może korzystać z takich Copy propagation CSE i constant folding)
+2) Sprawdź w jaki sposób kompilator obsługuje zmienne lokalne oraz globalne (gdzie oraz w jaki sposób są przechowywane, oraz jak są obsługiwane w trakcie trwania programu)
+3) Dodaj flagę optymalizacji np. O3 - czy są widoczne różnice pomiędzy traktowaniem zmiennych globalnych oraz lokalnych?
+4) Zamień zmienne globalne na lokalne, sprawdź kod assemblera z włączoną flagą O3, które linie kodu zostały wykonane w trakcie kompilacji? 
 */
